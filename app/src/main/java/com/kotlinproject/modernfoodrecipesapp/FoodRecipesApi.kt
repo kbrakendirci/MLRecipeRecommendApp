c205a0e0919f8b5316f672dcf7ccbee2b3a1de95
Kübra kendirci <kubrakendirci0@gmail.com>
2022-03-31 19:00:48 +0300

    DataSource

---
+++ app/src/main/java/com/kotlinproject/modernfoodrecipesapp/FoodRecipesApi.kt
@@ -0,0 +1,12 @@
+package com.kotlinproject.modernfoodrecipesapp
+import com.kotlinproject.modernfoodrecipesapp.model.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+}
