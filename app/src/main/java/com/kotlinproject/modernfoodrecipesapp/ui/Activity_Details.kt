e93cd9e560bb0cbd6c7a1365afa83cecc5ebefdb
Kübra kendirci <kubrakendirci0@gmail.com>
2022-04-04 20:54:33 +0300

    Update Activity_Details.kt

--- app/src/main/java/com/kotlinproject/modernfoodrecipesapp/ui/Activity_Details.kt
+++ app/src/main/java/com/kotlinproject/modernfoodrecipesapp/ui/Activity_Details.kt
@@ -2,6 +2,7 @@ package com.kotlinproject.modernfoodrecipesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.core.content.ContextCompat
 import com.kotlinproject.modernfoodrecipesapp.R
 import kotlinx.android.synthetic.main.activity_details.*
@@ -14,5 +15,13 @@ class Activity_Details : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+//Sayfaya geri dönüş için
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
