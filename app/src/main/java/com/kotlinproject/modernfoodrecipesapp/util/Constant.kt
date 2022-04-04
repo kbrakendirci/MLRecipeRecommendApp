9e178819bbd078e00b5847d3ca683938f447c5a5
Kübra kendirci <kubrakendirci0@gmail.com>
2022-04-04 23:47:36 +0300

    Ingredients Page edit

--- app/src/main/java/com/kotlinproject/modernfoodrecipesapp/util/Constant.kt
+++ app/src/main/java/com/kotlinproject/modernfoodrecipesapp/util/Constant.kt
@@ -4,9 +4,11 @@ class Constant{
 
     companion object {
 
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
+
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "a3206fe66742458b81063fe27ec66fc1"
-
+        const val RECIPE_RESULT_KEY = "recipeBundle"
         // API Query Keys
         const val QUERY_SEARCH="query"
         const val QUERY_NUMBER = "number"
